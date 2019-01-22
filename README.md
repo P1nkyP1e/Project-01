ff75902e80a21380b09f5d2d89ddd9061ce781e7
Darouny <rabiei@telus.net>
2019-01-22 08:58:24 -0800

    update css

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Aloha project
 This is a project to showcase HTML and CSS to build a responsive website as part of a Web Development Program at RED Academy.
+I have learnt a great deal during this project.
+Using proper Mark up for my html and css properties.
+Also, used "smooth scrolling" in the menu, Flickty/jquery for my carousel.
+Implemented an alert box for subscription.
+Used flexbox and their properties.
+I tried to optimized my layout with the use of required breakpoints for implementing mobile first layout.
+My biggest lesson was remembering to save my work as as i go along. As i lost so must progress in not saving my work.
+In say so, i could done a lot better by remembering to save my work.
--- aloha.js
+++ aloha.js
@@ -1,3 +1,7 @@
+
+//   carousel   //
+
+ 
 $(".main-carousel").flickity({
   draggable: true,
   freeScroll: true,
@@ -5,4 +9,38 @@ $(".main-carousel").flickity({
   cellAlign: "left",
   contain: true,
   autoPlay: true
+});
+
+
+// form-button  //
+
+
+
+$("form").on("click", "button", function(event) {
+  event.preventDefault();
+
+  alert("Thanks for subscribing!");
+});
+
+//  smooth scrolling source @ https://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
 });
